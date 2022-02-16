2e1150192bc3c483584f547f750ea46d908e5d6b
Ivan <vanya909x75@gmail.com>
2022-02-16 19:57:56 +0800

    Add django authentication system
    Also add log in, log out and sign up buttons to the home page

--- review_proj/urls.py
+++ review_proj/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
+    path('users/', include('django.contrib.auth.urls')),
     path('reviews/', include('reviews.urls')),
     path('', include('home.urls')),
 ]
